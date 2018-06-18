75392df846e47170279fa6f1e7c63f197e746d90
Justin W Hall <justinwhall@me.com>
2018-06-17 19:11:01 -0600

    more deprecated functions

--- services.php
+++ services.php
@@ -57,7 +57,7 @@ function hookpress_ajax_add_fields() {
 			'url'=>$_POST['url'],
 			'type'=>$_POST['type'],
 			'hook'=>$_POST['hook'],
-			'fields'=>split(',',$_POST['fields']),
+			'fields'=>explode(',',$_POST['fields']),
 			'enabled'=>true
 		);
 		$id = hookpress_add_hook($newhook);
